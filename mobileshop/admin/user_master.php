6136a2336bf534330d669b488843c9e2bb3d21f3
bhavik0111 <118987227+bhavik0111@users.noreply.github.com>
2023-01-24 18:22:17 +0530

    Update user_master.php

--- mobileshop/admin/user_master.php
+++ mobileshop/admin/user_master.php
@@ -7,6 +7,7 @@ $usr_email = '';
 $usr_email_error='';
 $usr_password = '';
 $usr_password_error='';
+$edit_usr_password='';
 $usr_role = '';
 $usr_role_error='';
 $usr_block = '';
@@ -31,67 +32,13 @@ if (isset($_GET['id'])){
 	if (isset($_GET["msg"]) && $_GET["msg"] == 'I') {
 		$submitedmsg = 'Record Added';
 	}
-	if (isset($_GET["msg"]) && $_GET["msg"] == 'u') {
+	if (isset($_GET["msg"]) && $_GET["msg"] == 'U') {
 		$submitedmsg = 'Record Updated';
 	}
-	if (isset($_GET["msg"]) && $_GET["msg"] == 'd') {
+	if (isset($_GET["msg"]) && $_GET["msg"] == 'D') {
 		$submitedmsg = 'Record Deleted';
 	}
 //End msg..........
-
-// insert in DB with validation..........................
-if (isset($_POST['submit'])) { $error = 'false';
-	
-    $usr_name = $_POST['usr_name'];
-		if ($usr_name == ''){
-			$usr_name_error = 'Please enter Name';
-			$error = 'true';
-		}
-    $usr_email = $_POST['usr_email'];
-		if ($usr_email == ''){
-			$usr_email_error = 'Please enter Email';
-			$error = 'true';
-		}
-    $usr_password = $_POST['usr_password'];
-		if ($usr_password == ''){
-			$usr_password_error = 'Please enter Password';
-			$error = 'true';
-		}
-    $usr_role = $_POST['usr_role'];
-		if ($usr_role == ''){
-			$usr_role_error = 'Please select Role';
-			$error = 'true';
-		}
-    $usr_block = $_POST['usr_block'];
-		if ($usr_block == ''){
-			$usr_block_error = 'Please enter this field';
-			$error = 'true';
-		}
-    $status = $_POST['status'];
-	    if($status == ''){
-			$status_error = 'Please enter status';
-			$error ='true';
-		}
-
-	if ($error == 'false') 
-    {
-		if($_POST["submit"] == 'Add') 
-        {
-			$ins_query ="INSERT INTO `user_master` (`usr_name`,`usr_email`,`usr_password`,`usr_role`,`usr_block`,`status`)VALUES ('" .$usr_name . "','" . $usr_email . "','" . $usr_password . "','" . $usr_role . "','" . $usr_block ."','" .$status ."')";
-			$ins_result = mysqli_query($conn, $ins_query);
-
-				header("location:user_master.php?msg=I");
-				//header("refresh:2");
-		}
-		elseif ($_POST["submit"] == 'Edit'){
-            $user_update = "UPDATE `user_master` SET `usr_name`='".$usr_name."', `usr_email`='" . $usr_email . "', `usr_password`='" . $usr_password . "', `usr_role`='" . $usr_role . "', `usr_block`='" . $usr_block ."',`status`='" .$status ."' WHERE usr_id='$id'";
-            $user_result = mysqli_query($conn, $user_update);
-
-				header("location:user_master.php?msg=u");
-			}
-	}
-}
-// End insert.............................
 
 // All query's.....................
 	$listing_sql = "SELECT * FROM `user_master` WHERE `usr_id` = usr_id " ;
@@ -109,111 +56,156 @@ if ($id != '') {
     $user_row = $update_result->fetch_assoc();
     $usr_name = $user_row['usr_name'];
     $usr_email = $user_row['usr_email'];
-    $usr_password = $user_row['usr_password'];
+    $edit_usr_password = $user_row['usr_password'];
     $usr_block = $user_row['usr_block'];
     $usr_role = $user_row['usr_role'];
 	$status = $user_row['status'];
 }
 // End edit form..........
 
-//Delete ................................
-/*
-if ($id != '' && $action == 'Delete') {
+// insert in DB with validation..........................
+if (isset($_POST['submit'])) { $error = 'false';
+	
+    $usr_name = $_POST['usr_name'];
+    $usr_email = $_POST['usr_email'];
+    $usr_password=$_POST['usr_password'];
+    $usr_role = $_POST['usr_role'];
+    $usr_block = $_POST['usr_block'];
+    $status = $_POST['status'];
 
+		if($usr_name == ''){
+			$usr_name_error = 'Please enter Name';
+			$error = 'true';
+		}
+    
+		if($usr_email == ''){
+			$usr_email_error = 'Please enter Email';
+			$error = 'true';
+		}
+	
+		if($usr_password == '' && $edit_usr_password==''){
+			$usr_password_error = 'Please enter Password';
+			$error = 'true';
+		}
+	
+		if ($usr_role == ''){
+			$usr_role_error = 'Please select Role';
+			$error = 'true';
+		}
+		if ($usr_block == ''){
+			$usr_block_error = 'Please enter this field';
+			$error = 'true';
+		}
+	    if($status == ''){
+			$status_error = 'Please enter status';
+			$error ='true';
+		}
+    // check exist email
+		if($usr_email != ''){
+			if($id!=''){
+				$fiend_email =mysqli_query($conn, "SELECT * FROM `user_master` WHERE usr_email='" . $usr_email . "' WHERE $id != '' ");
+	        	if(mysqli_num_rows($fiend_email)>0){ 
+	        		$usr_email_error = "email id already exists";
+		    	}	
+				}else{
+					$fiend_email =mysqli_query($conn, "SELECT * FROM `user_master` WHERE usr_email = '" . $usr_email . "' ");
+			        if(mysqli_num_rows($fiend_email)>0){ 
+			        $usr_email_error = "email id already exists";
+		    	}
+	    	}
+	    }
+    //End here exist email
+
+	if ($error == 'false'){
+		if($_POST["submit"] == 'Add'){
+			$ins_query ="INSERT INTO `user_master` (`usr_name`,`usr_email`,`usr_password`,`usr_role`,`usr_block`,`status`) VALUES ('" . $usr_name . "','" . $usr_email . "','" . md5($usr_password) . "','" . $usr_role . "','" . $usr_block ."','" . $status ."')";
+
+			$ins_result = mysqli_query($conn, $ins_query);
+			header("location:user_master.php?msg=I");
+		}
+		elseif($_POST["submit"] == 'Edit'){
+	        $user_update = "UPDATE `user_master` SET `usr_name`='".$usr_name."', `usr_email`='" . $usr_email . "', `usr_role`='" . $usr_role . "', `usr_block`='" . $usr_block ."',`status`='" .$status ."' WHERE usr_id='$id'";
+	        $user_result = mysqli_query($conn, $user_update);
+
+            if($usr_password != ''){
+            	$user_update = "UPDATE `user_master` SET `usr_password`='" .$usr_password ."' WHERE usr_id='$id'";
+	        	$user_result = mysqli_query($conn, $user_update);
+            }
+			header("location:user_master.php?msg=U");
+		}
+	}
+}
+// End insert....
+
+//Delete ....
+if ($id != '' && $action == 'Delete'){
     $delete_user_query  = "SELECT * FROM `user_master` WHERE `usr_id` =  $id ";
-	echo $delete_user_query;
-	exit;
+	// echo $delete_user_query;
+	// exit;
     $delete_user_result = mysqli_query($conn, $delete_user_query);
     $delete_user_row    = mysqli_num_rows($delete_user_result);
 
 	if ($delete_user_row > 0) {
-        $listing_delete  = mysqli_query($conn, "DELETE  FROM  `user_master` WHERE `usr_id`=" . $id);
-        header("location:user_master.php?msg=Delete");
+        $listing_delete  = mysqli_query($conn, "DELETE  FROM  `user_master` WHERE `usr_id`= $id");
+        header("location:user_master.php?msg=D");
     }
 }
-*/
 //End delete.........................
 
 ?>
 <!DOCTYPE html>
 <html>
 	<head>
-		<link href="./style.css"  rel="stylesheet">	
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">	
 	</head>
 	<body class="d-flex flex-column h-100">
-		<?php 
-			include('header.php');
-		
-if($action == 'Add' || $action == 'Edit')
-	{ 
-		?>
-		<div class="user_master.php" align="right">
-			<a href="user_master.php" class="btn btn-outline-success">Back</a>
-		</div>
-		<div class="container-fluid dash_cus mt-5 pt-5">
-			<div class="header text-center">
-				<h2>Add User</h2>
-			</div>
-			<div class="container">
-				<form method="POST" action="user_master.php?<?php echo $formaction; ?>">
-					<div class="row card p-5">
-						<div class="col-sm-12">
-							<div class="mb-3">
-							<label for="Image" class="form-label">Name</label>
-							<input class="form-control" type="text" id="name" name="usr_name" value="<?php echo $usr_name; ?>"><?php echo $usr_name_error ; ?>
- 							</div>
-						</div>
-						<div class="col-sm-12">
-							<div class="mb-3">
-							<label for="Image" class="form-label">Email</label>
-							<input class="form-control" type="email" id="email" name="usr_email" value="<?php echo $usr_email; ?>"><?php echo $usr_email_error ; ?>
-							</div>
-						</div>
-						<div class="col-sm-12">
-							<div class="mb-3">
-							<label for="Image" class="form-label">Password</label>
-							<input class="form-control" type="password" name="usr_password" id="usr_password" value="<?php echo $usr_password; ?>"><?php echo $usr_password_error ; ?>
-							</div>
-						</div>
-						<div class="col-sm-12">
-							<div class="mb-3">
-							<label for="Image" class="form-label">Block</label>
-							<input class="form-control" type="text" name="usr_block" id="" value="<?php echo $usr_block; ?>"><?php echo $usr_block_error ; ?>
-							</div>
-						</div>
-						<div class="col-sm-12">
-							<div class="mb-3">
-							<label for="Role" class="form-label">Role</label>
-							<select name="usr_role" class="form-control" value="<?php echo $usr_role; ?>"><?php echo $usr_role_error ; ?>
-							<option value="0">--select--</option>
-							<option value="Admin"<?php if ($usr_role == 'Admin') { echo 'selected="selected"'; } ?>>Admin</option>
-							<option value="Seller"<?php if ($usr_role == 'Seller') { echo 'selected="selected"'; } ?>>Seller</option>
-							<option value="Customer"<?php if ($usr_role == 'Customer') { echo 'selected="selected"'; } ?>>Customer</option></select>
-							</div>
-						</div>
-						<div class="col-sm-12">
-							<div class="mb-3">
-							<label for="Role" class="form-label">Status</label>
-							<br>
-								<div class="form-check form-check-inline">
-								<input class="form-check-input" type="radio" name="status" id="inlineRadio1" value="1" <?php if ($status == '1') { echo 'checked="checked"'; }?>>
-								<label class="form-check-label" for="inlineRadio1">Active</label>
-								</div>
-								<div class="form-check form-check-inline">
-								<input class="form-check-input" type="radio" name="status" id="inlineRadio2" value="0" <?php if ($status == '0') { echo 'checked="checked"'; }?>>
-								<label class="form-check-label" for="inlineRadio2">Deactive</label>
-								</div>
-							</div>
-						</div>
-						<div class="col-sm-12">
-							<div class="mb-3" align="center">
-								<input type="submit" class="btn btn-outline-success" name="submit" value="<?php echo $action; ?>">
-							</div>
-						</div>
-					</div>
-				</form>
-			</div>
-		</div>
+	<?php 
+if($action == 'Add' || $action == 'Edit'){ ?>
+	<div class="user_master.php" align="right">
+		<a href="user_master.php" class="btn btn-outline-success">Back</a>
+	</div>
+	<div class="header text-center">
+		<h2>User</h2>
+	</div>
+	<form method="POST" action="user_master.php?<?php echo $formaction; ?>">
+		<table align="center" border="1" cellspacing="0" cellpadding="15">
+			<tr>
+				<th>Name</th>
+				<td><input  type="text"  name="usr_name" value="<?php echo $usr_name; ?>"><br><?php echo $usr_name_error ; ?></td>
+			</tr>
+		    <tr>
+		    	<th>Email</th>
+		    	<td><input type="email"  name="usr_email" value="<?php echo $usr_email; ?>"><br><?php echo $usr_email_error ; ?></td>
+		    </tr>
+			<tr>
+				<th>Password</th>
+				<td><input type="password" name="usr_password" ><br><?php echo $usr_password_error ; ?></td>
+			</tr>
+			<tr>
+				<th>Block</th>
+				<td><input  type="text" name="usr_block"  value="<?php echo $usr_block; ?>"><br><?php echo $usr_block_error ; ?></td>
+			</tr>
+			<tr>
+				<th>Role</th>
+				<td>
+				   <select name="usr_role" value="<?php echo $usr_role; ?>"><br><?php echo $usr_role_error ; ?>
+					<option value="0">--select--</option>
+					<option value="Admin"<?php if ($usr_role == 'Admin') { echo 'selected="selected"'; } ?>>Admin</option>
+					<option value="Seller"<?php if ($usr_role == 'Seller') { echo 'selected="selected"'; } ?>>Seller</option>
+					<option value="Customer"<?php if ($usr_role == 'Customer') { echo 'selected="selected"'; } ?>>Customer</option></select>
+				</td>
+			</tr>
+		    <tr>
+		    	<th>Status</th>
+		    	<td><input type="radio" name="status" value="1" <?php if ($status == '1') { echo 'checked="checked"'; }?>>Active
+		    		<input type="radio" name="status" value="0" <?php if ($status == '0') { echo 'checked="checked"'; }?>>Deactive
+		    	</td>
+		    </tr>
+			<tr>
+				<td colspan="2" align="center"><input type="submit" name="submit" value="<?php echo $action; ?>"></td>
+			</tr>
+		</table>
+	</form>
   <?php 
 	} 
 	else{ 
@@ -235,7 +227,6 @@ if($action == 'Add' || $action == 'Edit')
                     <th>usr_id</th>
                     <th>usr_name</th>
                     <th>usr_email</th>
-                    <th>usr_password</th>
                     <th>usr_role</th>
                     <th>usr_block</th>
                     <th>status</th>
@@ -248,7 +239,6 @@ if($action == 'Add' || $action == 'Edit')
                             <td><?php echo $listing_row['usr_id']; ?></td>
                             <td><?php echo $listing_row['usr_name']; ?></td>
                             <td><?php echo $listing_row['usr_email']; ?></td>
-                            <td><?php echo $listing_row['usr_password']; ?></td>
                             <td><?php echo $listing_row['usr_role']; ?></td>
                             <td><?php echo $listing_row['usr_block']; ?></td>
                             <td><?php echo $listing_row['status']; ?></td>
@@ -264,8 +254,8 @@ if($action == 'Add' || $action == 'Edit')
 	<?php
 	}
   ?>
-		<?php 
-			// include('footer.php');
-		?>
 	</body>
 </html>
+	<?php 
+		// include('footer.php');
+	?>
