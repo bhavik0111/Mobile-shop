31ec25f4105e4f901c6066fbc2c98c7291a883fa
bhavik0111 <118987227+bhavik0111@users.noreply.github.com>
2023-01-30 18:15:19 +0530

    Update product.php

--- mobileshop/admin/product.php
+++ mobileshop/admin/product.php
@@ -5,10 +5,13 @@ $prod_id='';
 $prod_id_error='';
 $cat_id='';
 $cate_id_error='';
-$prod_image='';
-$prod_image_error='';
 $prod_name='';
 $prod_name_error='';
+$prod_price='';
+$prod_price_error='';
+$prod_image='';
+$prod_image_error='';
+$edit_prod_image='';
 $prod_desc='';
 $prod_desc_error='';
 $prod_status='';
@@ -18,13 +21,13 @@ $id = '';
 $formaction = '';
 $submitedmsg = '';
 
+$search_url = isset($_GET['search']) && $_GET['search'] ? 'search='.$_GET['search'].'&' : '';   
+$search_value = isset($_GET['search']) && $_GET['search'] ? $_GET['search'] : ''; 
+
 $column = isset($_GET['column']) && $_GET['column'] ? $_GET['column'] : 'prod_id';
 $sort_order = isset($_GET['order']) && strtolower($_GET['order']) == 'desc' ? 'DESC' : 'ASC';
 $asc_or_desc = $sort_order == 'ASC' ? 'desc' : 'asc';
 
-$search_url = isset($_GET['search']) && $_GET['search'] ? 'search='.$_GET['search'].'&' : '';   
-$search_value = isset($_GET['search']) && $_GET['search'] ? $_GET['search'] : ''; 
-                                                              
 
 if(isset($_GET['action'])){
 	$action = $_GET['action'];
@@ -63,28 +66,33 @@ if($id != ''&& $action == 'Edit'){
 	$update_result = mysqli_query($conn, $update_query);
     
 	$prod_row = $update_result->fetch_assoc();
+
 	$prod_name   = $prod_row['prod_name'];
+	$prod_price   = $prod_row['prod_price'];
 	$edit_prod_image  = $prod_row['prod_image'];
 	$prod_desc   = $prod_row['prod_desc'];
 	$prod_status = $prod_row['prod_status'];
-
-	// $path ='./Cateimage/'.$cat_row['cat_image']; 
-	// 	if(file_exists($path)){ unlink($path); //echo "File Successfully update.";
-    // 	} 
 }
 // End edit form..........
 
 // insert in DB with validation............
-if (isset($_POST['submit'])) {
+if (isset($_POST['submit'])){
 	$error = 'false';
-
+    
+    $cat_id = $_POST['cat_id'];
 	$prod_name = $_POST['prod_name'];
+	$prod_price = $_POST['prod_price'];
 	$prod_image = $_FILES['prod_image'];
 	$prod_desc = $_POST['prod_desc'];
 	$prod_status = $_POST['prod_status'];
-
+     
+    
 	if($prod_name == ''){
 		$prod_name_error = 'Please enter Name';
+		$error = 'true';
+	}
+	if($prod_price == ''){
+		$prod_price_error = 'Please enter Price';
 		$error = 'true';
 	}
 	if($prod_image == '' && $edit_prod_image == ''){
@@ -126,19 +134,42 @@ if (isset($_POST['submit'])) {
 	{
 		if ($_POST["submit"] == 'Add')
 		{
-			$ins_query = "INSERT INTO `product_master` (`prod_name`,`prod_image`,`prod_desc`,`prod_status`) VALUES ('" . $prod_name . "','" . $prod_image . "','" . $prod_desc . "','" . $prod_status . "')";
+			$ins_query = "INSERT INTO `product_master` (`cat_id`,`prod_name`,`prod_price`,`prod_image`,`prod_desc`,`prod_status`) VALUES ('" . $cat_id . "','" . $prod_name . "','" . $prod_price . "','" . $prod_image . "','" . $prod_desc . "','" . $prod_status . "')";
 			$ins_result = mysqli_query($conn, $ins_query);
 			header("location:".SITE_URL_ADMIN."./product.php?msg=I");
 		}
 		elseif($_POST["submit"] == 'Edit')
 		{
-			$prod_update = "UPDATE `product_master` SET `prod_name`='" . $prod_name . "',`prod_image`='" . $prod_image . "', `prod_desc`='" . $prod_desc . "', `prod_status`='" . $prod_status . "'  WHERE prod_id='$id'";
+			$prod_update = "UPDATE `product_master` SET `prod_name`='" . $prod_name . "',`prod_price`='" . $prod_price . "',`prod_image`='" . $prod_image . "', `prod_desc`='" . $prod_desc . "', `prod_status`='" . $prod_status . "'  WHERE prod_id='$id'";
 			$prod_result = mysqli_query($conn, $prod_update);
-           	header("location:".SITE_URL_ADMIN."./product.php?msg=U");   //right...
+           	header("location:".SITE_URL_ADMIN."./product.php?msg=U");   
 		}
 	}
 }
 // End insert....
+
+//Delete ....
+if($id != '' && $action == 'Delete'){
+	$delete_prod_query  = "SELECT * FROM `PRODUCT_master` WHERE `prod_id` =  $id ";
+	$delete_prod_result = mysqli_query($conn, $delete_prod_query);
+	$delete_prod_row_count   = mysqli_num_rows($delete_prod_result);
+	$delete_prodimage_row = $delete_prod_result->fetch_assoc();
+        
+	if($delete_prod_row_count > 0){
+		// $path ='./Cateimage/'.$delete_catimage_row['cat_image']; 
+		$path =SITE_ROOT_IMG.'/product/'.$delete_prodimage_row['prod_image'];
+		if(file_exists($path)){ unlink($path); //echo "File Successfully Delete.";
+        	} 
+		$listing_delete  = mysqli_query($conn, "DELETE  FROM  `product_master` WHERE `prod_id`= $id");
+		header("location:".SITE_URL_ADMIN."./product.php?msg=D");   //right.....
+	}
+}
+//End delete............
+
+//category dropdown......
+	$category_selectname = "SELECT * FROM `category_master` where `cat_status`='Active'";
+	$result = mysqli_query($conn, $category_selectname);
+//End.......
 ?>
 <tr><td width="100%">
 	<table width="100%" border="0">
@@ -149,23 +180,16 @@ if (isset($_POST['submit'])) {
 		<tr><td>
 			<form method="POST" action="product.php?<?php echo $formaction; ?>" enctype="multipart/form-data">
 				<table width="50%" border="1" cellpadding="5" align="center">
-					<tr><td colspan="2" align="right"><a href="<?php echo SITE_URL_ADMIN.'/product.php';?>" class="btn btn-outline-success">Back</a></td></tr>
-                    
-                    <?php 
-                        $category_selectname = "SELECT * FROM `category_master` WHERE `cat_id`='. $id .' AND `cat_status`='Active'";
-                        $result = mysqli_query($conn, $category_selectname);
-                    ?>
+					<tr><td colspan="2" align="right"><a href="<?php echo SITE_URL_ADMIN.'/product.php';?>"class="btn btn-outline-success">Back</a></td></tr>
                     <tr>
                         <th>Category</th>
                         <td>
                             <select name="cat_id" value="<?php echo $cat_id; ?>"><br><?php echo $cat_id_error; ?>
                                 <option value="0">--select--</option>
-                                <?php
-                                if ($result->num_rows > 0) {
-                                    while ($listing_row = $result->fetch_assoc()) 
-                                    {
-                                        ?>
-                                <option value="<?php echo $listing_row['cat_id']; ?>" <?php if ($cat_name == $listing_row['cat_id']){ echo "selected"; }
+                            <?php if($result->num_rows > 0){
+                               		while ($listing_row = $result->fetch_assoc()){ 
+                               			?>
+                                <option value="<?php echo $listing_row['cat_id']; ?>" <?php if ($id == $listing_row['cat_id']){ echo "selected"; }
                                                     ?>><?php echo $listing_row['cat_name']; ?></option>
                                         <?php
                                     }
@@ -179,6 +203,10 @@ if (isset($_POST['submit'])) {
 						<td><input type="text" name="prod_name" value="<?php echo $prod_name; ?>"><br><?php echo $prod_name_error; ?></td>
 					</tr>
 					<tr>
+						<th>Price</th>
+						<td><input type="text" name="prod_price" value="<?php echo $prod_price; ?>"><br><?php echo $prod_price_error; ?></td>
+					</tr>
+					<tr>
 						<th>Image</th>
 						<td><input type="file" name="prod_image" value=""><br><?php echo $prod_image_error; ?></td>
 					</tr>
@@ -188,8 +216,8 @@ if (isset($_POST['submit'])) {
 					</tr>
 					<tr>
 						<th>Status</th>
-						<td><input type="radio" name="prod_status" value="1" <?php echo "checked $prod_status == '1'" ; ?>>Active
-							<input type="radio" name="prod_status" value="0" <?php if($prod_status == '0'){ echo 'checked="checked"'; } ?>>Deactive
+						<td><input type="radio" name="prod_status" value="Active" <?php echo "checked $prod_status == 'Active'" ; ?>>Active
+							<input type="radio" name="prod_status" value="Deactive" <?php if($prod_status == 'Deactive'){ echo "checked"; } ?>>Deactive
 						</td>
 					</tr>
 					<tr>
@@ -213,25 +241,29 @@ if (isset($_POST['submit'])) {
 		<tr><td>
 			<table width="100%" border="1">
 				<tr>
-					<td><a href="product.php?<?php echo $search_url; ?>column=prod_id&order=<?php echo $asc_or_desc; ?>">ID</td>
-					<td><a href="product.php?<?php echo $search_url; ?>column=prod_name&order=<?php echo $asc_or_desc; ?>">Name</td>
-					<td>Image</td>
-					<td><a href="product.php?<?php echo $search_url; ?>column=prod_desc&order=<?php echo $asc_or_desc; ?>">Description</td>
-					<td><a href="product.php?<?php echo $search_url; ?>column=prod_status&order=<?php echo $asc_or_desc; ?>">Status</td>
-					<td>Action</td>
+					<td><a href="product.php?<?php echo $search_url; ?>column=prod_id&order=<?php echo $asc_or_desc; ?>"><b>ID</b></td>
+					<td><b>Category id</b></td>
+					<td><a href="product.php?<?php echo $search_url; ?>column=prod_name&order=<?php echo $asc_or_desc; ?>"><b>Name</b></td>
+					<td><a href="product.php?<?php echo $search_url; ?>column=prod_price&order=<?php echo $asc_or_desc; ?>"><b>Price</b></td>
+					<td><b>Image</b></td>
+					<td><a href="product.php?<?php echo $search_url; ?>column=prod_desc&order=<?php echo $asc_or_desc; ?>"><b>Description</b></td>
+					<td><a href="product.php?<?php echo $search_url; ?>column=prod_status&order=<?php echo $asc_or_desc; ?>"><b>Status</b></td>
+					<td><b>Action</b></td>
 				</tr>
 				<?php
 					if($listing_result->num_rows > 0){
 					 	while($listing_row = $listing_result->fetch_assoc()){ ?>
 				<tr>
 					<td><?php echo $listing_row['prod_id']; ?></td>
+					<td><?php echo $listing_row['cat_id']; ?></td>
 					<td><?php echo $listing_row['prod_name']; ?></td>
+					<td><?php echo $listing_row['prod_price']; ?></td>
 					<td><img src="<?php echo SITE_URL_IMG.'/product/'.$listing_row['prod_image']; ?>" height="100" width="100"></td>
 					<td><?php echo $listing_row['prod_desc']; ?></td>
 					<td><?php echo $listing_row['prod_status']; ?></td>
 					<td>
 						<a class="btn btn-info" href="product.php?action=Edit&id=<?php echo $listing_row['prod_id']; ?>">Edit</a>&nbsp;
-						<a class="btn btn-danger" value="Delete" href="product.php?action=Delete&id=<?php echo $listing_row['prod_id']; ?>">Delete</a>
+						<a class="btn btn-danger" onclick="return deletelist();" value="Delete" href="product.php?action=Delete&id=<?php echo $listing_row['prod_id']; ?>">Delete</a>
 					</td>
 				</tr>
 				<?php } } ?>
@@ -241,7 +273,16 @@ if (isset($_POST['submit'])) {
 		<?php  	
 		}
 		?>
-
+     <script>
+	    //conformation delete msg.......
+	    function deletelist(){
+	        if (confirm("Are you sure Delete Rocord?")){
+	            return true;
+	        }else{
+	            return false;
+	        }
+	    }
+    </script>
 	</table>
 </td></tr> 
 <?php include('footer.php'); ?>   
