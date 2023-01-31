ace853614d5c16baa156bd3d2f20ee55d9c7b4c9
bhavik0111 <118987227+bhavik0111@users.noreply.github.com>
2023-01-31 12:20:43 +0530

    Update header.php

--- mobileshop/admin/header.php
+++ mobileshop/admin/header.php
@@ -1,7 +1,7 @@
 <?php include 'connect.php';?>
 <!DOCTYPE html>
 <html>
-<head>
+<head> 
 </head>
 <body>
 
@@ -10,7 +10,12 @@
 		<table width="100%" align="center" border="1">
 			<tr><td width="100%" align="center"><h1>BhavikShop</h1></td>
 		</tr><tr>
-			<td><ul style="display: flex;"><li><a href="<?php echo SITE_URL_ADMIN;?>/user_master.php">Users</a></li><li><a href="<?php echo SITE_URL_ADMIN;?>/category.php">Category</a></li></ul></td>
+			<td><ul style="display: flex;">
+					<li><a href="<?php echo SITE_URL_ADMIN;?>/user_master.php"><b>Users</b></a></li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					<li><a href="<?php echo SITE_URL_ADMIN;?>/category.php"><b>Category</b></a></li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					<li><a href="<?php echo SITE_URL_ADMIN;?>/product.php"><b>Product</b></a></li>
+				</ul>
+			</td>
 			
 		</tr></table>
 		
