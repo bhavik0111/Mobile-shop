98ff386f6bc03ba5a8ab889df0a74dec909067ab
bhavik0111 <118987227+bhavik0111@users.noreply.github.com>
2023-02-06 09:01:00 +0530

    Update header.php

--- mobileshop/admin/header.php
+++ mobileshop/admin/header.php
@@ -1,7 +1,12 @@
 <?php
- 	include 'connect.php';
  	session_start();
  	require "function.php";
+ 	include 'connect.php';
+
+ 	
+ 	    if(check_login_status() == false){
+			header("location:login.php");
+	    }
  ?>
 <!DOCTYPE html>
 <html>
@@ -14,7 +19,9 @@
 			<tr><td width="100%" align="center"><h1>BhavikShop</h1></td></tr>
 			<tr><td>
 				<ul style="display: flex;">
+				<?php if($_SESSION["usr_role"] == 'Admin') { ?>
 					<li><a href="<?php echo SITE_URL_ADMIN;?>/user_master.php"><b>Users</b></a></li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				<?php } ?>
 					<li><a href="<?php echo SITE_URL_ADMIN;?>/category.php"><b>Category</b></a></li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 					<li><a href="<?php echo SITE_URL_ADMIN;?>/product.php"><b>Product</b></a></li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 					<li><a href="<?php echo SITE_URL_ADMIN;?>/logout.php"><b>Logout</b></a></li>
